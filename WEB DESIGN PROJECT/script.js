b47dcbd4c5f6cfed8350366d1f06860acb7852fd
KehindeBanwo22 <105207459+KehindeBanwo22@users.noreply.github.com>
2024-01-22 00:20:02 -0800

    Add files via upload

---
+++ WEB DESIGN PROJECT/script.js
@@ -0,0 +1,10 @@
+// const logo = document.querySelector('.logo');
+// const nav = document.querySelector('.navigation');
+
+// logo.addEventListener('click', () => {
+//     if (nav.style.transform === 'scaleX(1)') {
+//         nav.style.transform = 'scaleX(0)';
+//     } else {
+//         nav.style.transform = 'scaleX(1)';
+//     }
+// });
